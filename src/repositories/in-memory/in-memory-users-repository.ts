6ae62f6d1e66abed90c705c79dc13b41a6a6c50d
Bruno <bruno.silva.castilho@gmail.com>
2025-04-16 13:39:44 -0300

    Caso de uso de perfil

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -4,6 +4,14 @@ import { UsersRepository } from '../users-repository'
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
 
+  findById(id: string): Promise<User | null> {
+    const user = this.items.find((item) => item.id === id)
+
+    if (!user) return null
+
+    return user
+  }
+
   findByEmail(email: string): Promise<User | null> {
     const user = this.items.find((item) => item.email === email)
 
